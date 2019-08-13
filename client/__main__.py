cf5f2b8c09ba1989e9aee09148b372670a9050d0
SAMoskalenko <41867418+SAMoskalenko@users.noreply.github.com>
2019-08-13 21:58:29 +0300

    hw2 первый вариант

--- client/__main__.py
+++ client/__main__.py
@@ -1,19 +1,7 @@
 import yaml
-import json
-import zlib
-import hashlib
-import threading
-from socket import socket
+from app_client import Application
 from argparse import ArgumentParser
-from datetime import datetime as dt
-
-
-def read(sock, buffersize):
-    while True:
-        response = sock.recv(buffersize)
-        server_response = zlib.decompress(response)
-        print(server_response.decode())
-
+from config import Config
 
 parser = ArgumentParser()
 
@@ -24,45 +12,15 @@ parser.add_argument(
 
 args = parser.parse_args()
 
-base_settings = {
-    'host': 'localhost',
-    'port': 8000,
-    'buffersize': 1024,
-}
-
 if args.settings:
     with open(args.settings) as file:
         settings = yaml.load(file, Loader=yaml.Loader)
-        base_settings.update(settings)
+        conf = Config(settings['host'], settings['port'], 1024)
+else:
+    conf = Config
 
-client = socket()
-client.connect((base_settings.get('host'), base_settings.get('port')))
+host, port, buffersize = conf.host, conf.port, conf.buffersize
 
-print('Client was started')
-
-try:
-    while True:
-        read_thread = threading.Thread(target=read, args=(client, base_settings.get('buffersize')))
-        read_thread.start()
-
-        hash_obj = hashlib.sha256()
-        hash_obj.update(str(dt.now().timestamp()).encode())
-
-        action = input('Enter action: ')
-        data = input('Enter message: ')
-
-        request = {
-            'action': action,
-            'data': data,
-            'token': hash_obj.hexdigest(),
-            'time': dt.now().timestamp()
-        }
-
-        client_request = json.dumps(request)
-        server_request = zlib.compress(client_request.encode())
-
-        client.send(server_request)
-        print(f'Client send data: {data}')
-except KeyboardInterrupt:
-    client.close()
-    print('Client shutdown')
+app = Application(host, port, buffersize)
+app.bind()
+app.start()
--- server/__main__.py
+++ server/__main__.py
@@ -1,27 +1,10 @@
 import yaml
 import logging
-import select
-import threading
-from socket import socket
+
+from app_server import Application
 from argparse import ArgumentParser
 from handlers import handle_default_request
-
-
-def read(sock, connections, requests, bufersize):
-    try:
-        client_request = sock.recv(bufersize)
-    except Exception:
-        connections.remove(sock)
-    else:
-        requests.append(client_request)
-
-
-def write(sock, connections, response):
-    try:
-        sock.send(response)
-    except Exception:
-        connections.remove(sock)
-
+from config import Config
 
 parser = ArgumentParser()
 
@@ -32,19 +15,14 @@ parser.add_argument(
 
 args = parser.parse_args()
 
-base_settings = {
-    'host': 'localhost',
-    'port': 8000,
-    'buffersize': 1024
-}
-
 if args.settings:
     with open(args.settings) as file:
         settings = yaml.load(file, Loader=yaml.Loader)
-        base_settings.update(settings)
+        conf = Config(settings['host'], settings['port'], 1024)
+else:
+    conf = Config
 
-host, port = base_settings.get('host'), base_settings.get('port')
-
+host, port, buffersize = conf.host, conf.port, conf.buffersize
 logging.basicConfig(
     level=logging.DEBUG,
     format='%(asctime)s - %(levelname)s - %(message)s',
@@ -54,40 +32,6 @@ logging.basicConfig(
     ]
 )
 
-connections = []
-requests = []
-
-try:
-    server = socket()
-    server.bind((host, port))
-    server.setblocking(False)
-    server.listen(5)
-
-    logging.info(f'Server was started with {host}:{port}')
-
-    while True:
-        try:
-            client, address = server.accept()
-            connections.append(client)
-            logging.info(f'Client was connected with {address[0]}:{address[1]} | Connections: {connections}')
-        except:
-            pass
-
-        rlist, wlist, xlist = select.select(connections, connections, connections, 0)
-
-        for r in rlist:
-            read_thread = threading.Thread(target=read,
-                                           args=(r, connections, requests, base_settings.get('buffersize')))
-            read_thread.start()
-
-        if requests:
-            client_request = requests.pop()
-            client_response = handle_default_request(client_request)
-
-            for w in wlist:
-                write_thread = threading.Thread(target=write,
-                                                args=(w, connections, client_response))
-                write_thread.start()
-
-except KeyboardInterrupt:
-    logging.info('Server shutdown')
+app = Application(host, port, buffersize, handle_default_request)
+app.bind()
+app.start()
